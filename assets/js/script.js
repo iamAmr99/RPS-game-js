edb1e4a6f06c9f21d6639485988a5ff05986e8cd
iamAmr99 <amradel5599@gmail.com>
2023-12-15 07:11:18 +0200

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 let bossChoice = document.getElementById('bossChoice')
-let bossScore = document.getElementById('bossScore').innerHTML
+let bossScoreView = document.getElementById('bossScoreView')
 let bossStatus = document.getElementById('bossStatus')
 let bossStatusColor = document.getElementById('bossStatusColor')
-let playerScore = document.getElementById('playerScore').innerHTML
+let playerScoreView = document.getElementById('playerScoreView')
 let playerStatus = document.getElementById('playerStatus')
 let playerStatusColor = document.getElementById('playerStatusColor')
 
 let playerHand = document.getElementById('playerHand')
+
+let playerScore = 0;
+let bossScore = 0;
 
 var userChoice;
 document.getElementById('rock').onclick = user;
@@ -35,82 +38,80 @@ function game(player, boss) {
         playerHand.src = 'assets/imgs/sprites/rock.gif'
         playerHand.style
         if (boss == 'scissors') {
-            bossStatusColor.classList.remove('bg-success')
-            bossStatusColor.classList.add('bg-danger')
-            bossStatus.innerHTML = 'Loser'
-            playerStatusColor.classList.remove('bg-danger')
-            playerStatusColor.classList.add('bg-success')
-            playerStatus.innerHTML = 'Winner'
+           playerWins()
         }
         else if (boss == 'paper') {
-            bossStatusColor.classList.remove('bg-danger')
-            bossStatusColor.classList.add('bg-success')
-            bossStatus.innerHTML = 'Winner'
-            playerStatusColor.classList.remove('bg-success')
-            playerStatusColor.classList.add('bg-danger')
-            playerStatus.innerHTML = 'Loser'
+           bossWins()
         } else if (boss == 'rock') {
-            bossStatusColor.classList.remove('bg-success', 'bg-danger')
-            bossStatusColor.classList.add('bg-primary')
-            bossStatus.innerHTML = 'Tie'
-            playerStatusColor.classList.remove('bg-success', 'bg-danger')
-            playerStatusColor.classList.add('bg-primary')
-            playerStatus.innerHTML = 'Tie'
+           tie()
         }
     }
     else if (player == 'paper') {
         playerHand.src = 'assets/imgs/sprites/paper.gif'
         if (boss == 'rock') {
-            bossStatusColor.classList.remove('bg-success')
-            bossStatusColor.classList.add('bg-danger')
-            bossStatus.innerHTML = 'Loser'
-            playerStatusColor.classList.remove('bg-danger')
-            playerStatusColor.classList.add('bg-success')
-            playerStatus.innerHTML = 'Winner'
-            playerScore = +1
+            playerWins()
         }
         else if (boss == 'scissors') {
-            bossStatusColor.classList.remove('bg-danger')
-            bossStatusColor.classList.add('bg-success')
-            bossStatus.innerHTML = 'Winner'
-            bossScore = +1
-            playerStatusColor.classList.remove('bg-success')
-            playerStatusColor.classList.add('bg-danger')
-            playerStatus.innerHTML = 'Loser'
+            bossWins()
         } else if (boss == 'paper') {
-            bossStatusColor.classList.remove('bg-success', 'bg-danger')
-            bossStatusColor.classList.add('bg-primary')
-            bossStatus.innerHTML = 'Tie'
-            playerStatusColor.classList.remove('bg-success', 'bg-danger')
-            playerStatusColor.classList.add('bg-primary')
-            playerStatus.innerHTML = 'Tie'
+            tie()
         }
     } else if (player == 'scissors') {
         playerHand.src = 'assets/imgs/sprites/scissors.gif'
         if (boss == 'paper') {
-            bossStatusColor.classList.remove('bg-success')
-            bossStatusColor.classList.add('bg-danger')
-            bossStatus.innerHTML = 'Loser'
-            playerStatusColor.classList.remove('bg-danger')
-            playerStatusColor.classList.add('bg-success')
-            playerStatus.innerHTML = 'Winner'
-            playerScore = +1
+            playerWins()
         }
         else if (boss == 'rock') {
-            bossStatusColor.classList.remove('bg-danger')
-            bossStatusColor.classList.add('bg-success')
-            bossStatus.innerHTML = 'Winner'
-            bossScore = +1
-            playerStatusColor.classList.remove('bg-success')
-            playerStatusColor.classList.add('bg-danger')
-            playerStatus.innerHTML = 'Loser'
+            bossWins()
         } else if (boss == 'scissors') {
-            bossStatusColor.classList.remove('bg-success', 'bg-danger')
-            bossStatusColor.classList.add('bg-primary')
-            bossStatus.innerHTML = 'Tie'
-            playerStatusColor.classList.remove('bg-success', 'bg-danger')
-            playerStatusColor.classList.add('bg-primary')
-            playerStatus.innerHTML = 'Tie'
+            tie()
         }
     }
+}
+
+function playerWins() {
+    playerScore += 1
+    bossStatusColor.classList.remove('bg-success')
+    bossStatusColor.classList.add('bg-danger')
+    bossStatus.innerHTML = 'Loser'
+    playerStatusColor.classList.remove('bg-danger')
+    playerStatusColor.classList.add('bg-success')
+    playerStatus.innerHTML = 'Winner'
+    updateScore()
+}
+function bossWins() {
+    bossScore += 1
+    bossStatusColor.classList.remove('bg-danger')
+    bossStatusColor.classList.add('bg-success')
+    bossStatus.innerHTML = 'Winner'
+    playerStatusColor.classList.remove('bg-success')
+    playerStatusColor.classList.add('bg-danger')
+    playerStatus.innerHTML = 'Loser'
+    updateScore()
+}
+function tie() {
+    bossStatusColor.classList.remove('bg-success', 'bg-danger')
+    bossStatusColor.classList.add('bg-primary')
+    bossStatus.innerHTML = 'Tie'
+    playerStatusColor.classList.remove('bg-success', 'bg-danger')
+    playerStatusColor.classList.add('bg-primary')
+    playerStatus.innerHTML = 'Tie'
+    updateScore()
+}
+function updateScore() {
+    playerScoreView.innerHTML = playerScore;
+    bossScoreView.innerHTML = bossScore;
+if (playerScore == 11) {
+    resetScore()
+    alert('Player saved the day !!!')
+} else if(bossScore == 11) {
+    resetScore()
+alert('You couldn\'t stop the Boss')
+}
+}
+function resetScore() {
+    playerScore = 0;
+    bossScore = 0;
+    playerScoreView.innerHTML = playerScore;
+    bossScoreView.innerHTML = bossScore;
 }
